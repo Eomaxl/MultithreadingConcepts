4235e6fae2921b6a5501019ea75ed6116d23e3fa
Eomaxl <souravsnigdh@gmail.com>
2025-08-25 20:54:42 +0530

    Sequential Execution

---
+++ src/main/java/com/eomaxl/SequentialExecutionDemo/SequentialExecutionDemo.java
@@ -0,0 +1,20 @@
+package com.eomaxl.SequentialExecutionDemo;
+
+public class SequentialExecutionDemo {
+    public static void main(String[] args) {
+        demo1();
+        demo2();
+    }
+
+    private static void demo1() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("From demo 1 :"+i);
+        }
+    }
+
+    private static void demo2() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("From demo 2 :"+i);
+        }
+    }
+}
